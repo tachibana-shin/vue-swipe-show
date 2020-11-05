039039ae3c9211fe3099a13ea1e0653c8783d6a2
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2020-11-05 16:05:57 +0700

    .

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const { resolve } = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+   entry: resolve(__dirname, 'src/index.js'),
+   output: {
+      path: resolve(__dirname, 'dist'),
+      filename: 'vue-nav-auto.js'
+   },
+   plugins: [
+        new UglifyJsPlugin({
+         exclude: [/\.min\.js$/gi] // skip pre-minified libs
+      }),
+          // make sure to include the plugin!
+          new VueLoaderPlugin()
+    ],
+   module: {
+      rules: [
+         {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+               loader: 'babel-loader',
+               options: {
+                  presets: ['@babel/preset-env'],
+                  plugins: ['@babel/plugin-proposal-object-rest-spread']
+               }
+            }
+         },
+         {
+            test: /\.vue$/,
+            loader: 'vue-loader'
+         }
+      ]
+   }
+}
